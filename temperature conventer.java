91474bce848a41e924b5219bd7dee19daf3e5e41
Keerthanakeerthana134 <146558676+Keerthanakeerthana134@users.noreply.github.com>
2026-01-28 10:49:31 +0530

    Add temperature converter functionality
    Java projects developed during my Software Development Internship at Prodigy InfoTech (Aug 2024). Includes a Temperature Converter and a Contact Management System."

---
+++ temperature conventer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Temperature Converter ---");
+        System.out.println("1. Celsius to Fahrenheit");
+        System.out.println("2. Fahrenheit to Celsius");
+        System.out.print("Choose option (1 or 2): ");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            System.out.print("Enter temp in Celsius: ");
+            double celsius = sc.nextDouble();
+            double fahrenheit = (celsius * 9/5) + 32;
+            System.out.println("Result: " + fahrenheit + "°F");
+        } else if (choice == 2) {
+            System.out.print("Enter temp in Fahrenheit: ");
+            double fahrenheit = sc.nextDouble();
+            double celsius = (fahrenheit - 32) * 5/9;
+            System.out.println("Result: " + celsius + "°C");
+        } else {
+            System.out.println("Invalid choice!");
+        }
+        sc.close();
+    }
+}
